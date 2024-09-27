445aa1cca4efd9465cdc6ef182b02457911fd162
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-09-27 20:30:12 +0500

    events-api: tickets: error handling added

--- events-api/src/modules/tickets/tickets.service.ts
+++ events-api/src/modules/tickets/tickets.service.ts
@@ -2,32 +2,34 @@ import { CreateTicketDto } from "./dto/create-ticket.dto";
 import { TicketModel } from "./tickets.model";
 import { TicketsRepository } from "./tickets.repository";
 
-
 export class TicketsService {
-
-    static async getTicketsByEventId(eventId: string): Promise<TicketModel[]> {
-        const tickets = await TicketsRepository.getByEventId(eventId);
-
-        return tickets;
+  static async getTicketsByEventId(eventId: string): Promise<TicketModel[]> {
+    try {
+      const tickets = await TicketsRepository.getByEventId(eventId);
+      return tickets;
+    } catch (err) {
+      console.error("TicketsServiceError: ", err);
+      throw new Error();
     }
+  }
 
-    static async create(ticket: CreateTicketDto) {
-        try {
-            const result = await TicketsRepository.create(ticket);
-            return result;
-        } catch (err) {
-            console.error("TicketsServiceError: couldn't create ticket: ", err);
-            throw new Error();
-        }
+  static async create(ticket: CreateTicketDto) {
+    try {
+      const result = await TicketsRepository.create(ticket);
+      return result;
+    } catch (err) {
+      console.error("TicketsServiceError: couldn't create ticket: ", err);
+      throw new Error();
     }
+  }
 
-    static async getTicketById(ticketId: string) {
-        try {
-            const ticketWithEvent = await TicketsRepository.getById(ticketId);
-            return ticketWithEvent;
-        } catch (err) {
-            console.error("TicketsServiceError: couldn't get ticket by id: ", err);
-            throw new Error();
-        }
+  static async getTicketById(ticketId: string) {
+    try {
+      const ticketWithEvent = await TicketsRepository.getById(ticketId);
+      return ticketWithEvent;
+    } catch (err) {
+      console.error("TicketsServiceError: couldn't get ticket by id: ", err);
+      throw new Error();
     }
+  }
 }
