869ac5c1f310c48442cbd2c844fec0b11371c667
Savesh1508 <savlatxon20042005@gmail.com>
2024-09-27 17:20:37 +0500

    Added ticket migrations

--- events-api/src/database/migrations/1726834021218-create-events.sql
+++ events-api/src/database/migrations/1726834021218-create-events.sql
@@ -9,5 +9,15 @@ CREATE TABLE IF NOT EXISTS events (
     event_updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE IF NOT EXISTS tickets (
+    ticket_id TEXT PRIMARY KEY,
+    ticket_quantity INTEGER NOT NULL,
+    ticket_price DECIMAL(10, 2) NOT NULL,
+    event_id TEXT NOT NULL,
+    FOREIGN KEY (event_id) REFERENCES events(event_id)
+);
+
+
 -- DOWN
 DROP TABLE IF EXISTS events;
+DROP TABLE IF EXISTS tickets;
