b4de20e3a29d4471778303f6d616f07abb8d8c61
purple-mountain <lmwrong92@gmail.com>
2024-10-03 10:47:19 +0000

    fix: updated create events schema error messages

--- events-api/src/modules/events/dto/requests/events-create-request-body.dto.ts
+++ events-api/src/modules/events/dto/requests/events-create-request-body.dto.ts
@@ -4,19 +4,19 @@ import { z } from "zod";
 export const eventsCreateRequestBodyDtoSchema = z
   .object({
     eventName: z
-      .string()
+      .string({ message: "Name is required" })
       .min(1, { message: "Name is required" })
       .max(70, { message: "Max length is 70" }),
     eventDescription: z
-      .string()
+      .string({ message: "Description is required" })
       .min(1, { message: "Description is required" })
       .max(1000, { message: "Max length is 1000" }),
     eventLocation: z
-      .string()
+      .string({ message: "Location is required" })
       .min(1, { message: "Location is required" })
       .max(70, { message: "Max length is 70" }),
     eventDate: z
-      .string()
+      .string({ message: "Date is required" })
       .min(1, { message: "Date is required" })
       .regex(eventDateRegex, {
         message: "Date must match YYYY-MM-DDTHH:mm format",
