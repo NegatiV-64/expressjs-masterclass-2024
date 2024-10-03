4e92ef620c983fda14f79703198298ca64788469
purple-mountain <lmwrong92@gmail.com>
2024-10-03 10:29:26 +0000

    fix: made tickets and events create schema strict

--- events-api/src/modules/events/dto/requests/events-create-request-body.dto.ts
+++ events-api/src/modules/events/dto/requests/events-create-request-body.dto.ts
@@ -1,26 +1,28 @@
 import { eventDateRegex } from "#/shared/constants/eventDateRegex";
 import { z } from "zod";
 
-export const eventsCreateRequestBodyDtoSchema = z.object({
-  eventName: z
-    .string()
-    .min(1, { message: "Name is required" })
-    .max(70, { message: "Max length is 70" }),
-  eventDescription: z
-    .string()
-    .min(1, { message: "Description is required" })
-    .max(1000, { message: "Max length is 1000" }),
-  eventLocation: z
-    .string()
-    .min(1, { message: "Location is required" })
-    .max(70, { message: "Max length is 70" }),
-  eventDate: z
-    .string()
-    .min(1, { message: "Date is required" })
-    .regex(eventDateRegex, {
-      message: "Date must match YYYY-MM-DDTHH:mm format",
-    }),
-});
+export const eventsCreateRequestBodyDtoSchema = z
+  .object({
+    eventName: z
+      .string()
+      .min(1, { message: "Name is required" })
+      .max(70, { message: "Max length is 70" }),
+    eventDescription: z
+      .string()
+      .min(1, { message: "Description is required" })
+      .max(1000, { message: "Max length is 1000" }),
+    eventLocation: z
+      .string()
+      .min(1, { message: "Location is required" })
+      .max(70, { message: "Max length is 70" }),
+    eventDate: z
+      .string()
+      .min(1, { message: "Date is required" })
+      .regex(eventDateRegex, {
+        message: "Date must match YYYY-MM-DDTHH:mm format",
+      }),
+  })
+  .strict({ message: "Invalid body" });
 
 export type EventsCreateRequestBodyDto = z.infer<
   typeof eventsCreateRequestBodyDtoSchema
--- events-api/src/modules/tickets/dto/requests/tickets-create-request-body.dto.ts
+++ events-api/src/modules/tickets/dto/requests/tickets-create-request-body.dto.ts
@@ -1,15 +1,17 @@
 import { z } from "zod";
 
-export const ticketsCreateRequestBodyDtoSchema = z.object({
-  ticketQuantity: z
-    .number({ message: "Ticket quantity must be number" })
-    .int({ message: "Ticket quantity must be integer" })
-    .positive({ message: "Ticket quantity must be positive" }),
-  ticketPrice: z
-    .number({ message: "Ticket price must be number" })
-    .positive({ message: "Ticket price must be positive" }),
-  eventId: z.string().uuid({ message: "Event ID is not a valid uuid" }),
-});
+export const ticketsCreateRequestBodyDtoSchema = z
+  .object({
+    ticketQuantity: z
+      .number({ message: "Ticket quantity must be number" })
+      .int({ message: "Ticket quantity must be integer" })
+      .positive({ message: "Ticket quantity must be positive" }),
+    ticketPrice: z
+      .number({ message: "Ticket price must be number" })
+      .positive({ message: "Ticket price must be positive" }),
+    eventId: z.string().uuid({ message: "Event ID is not a valid uuid" }),
+  })
+  .strict({ message: "Invalid body" });
 
 export type TicketsCreateRequestBodyDto = z.infer<
   typeof ticketsCreateRequestBodyDtoSchema
